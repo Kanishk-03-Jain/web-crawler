630ca10feed15f3ca857c31bd97c7da94b4db11a
Kanishk Jain <kanishkjain03082005@gmail.com>
2025-12-29 02:10:30 +0530

    writing main function

--- CrawlerStats.go
+++ CrawlerStats.go
@@ -11,12 +11,12 @@ type CrawledStats struct {
 	startTime             time.Time
 }
 
-func (c *CrawledStats) Update(crawled *CrawledSet, queue *Queue, t time.Time) {
-	c.pagesPerMinute += fmt.Sprintf("%f %d\n", t.Sub(c.startTime).Minutes(), crawled.Size())
-	c.crawledRatioPerMinute += fmt.Sprintf("%f %f\n", t.Sub(c.startTime).Minutes(), float64(crawled.Size())/float64(queue.Size()))
+func (c *CrawledStats) update(crawled *CrawledSet, queue *Queue, t time.Time) {
+	c.pagesPerMinute += fmt.Sprintf("%f %d\n", t.Sub(c.startTime).Minutes(), crawled.size())
+	c.crawledRatioPerMinute += fmt.Sprintf("%f %f\n", t.Sub(c.startTime).Minutes(), float64(crawled.size())/float64(queue.size()))
 }
 
-func (c *CrawledStats) Print() {
+func (c *CrawledStats) print() {
 	fmt.Println("Pages crawled per minute:")
 	fmt.Println(c.pagesPerMinute)
 	fmt.Println("Crawl to Queued Ratio per minute:")
--- main.go
+++ main.go
@@ -1,8 +1,68 @@
 package main
 
-import "github.com/Kanishk-03-Jain/web-crawler/queue"
+import (
+	"fmt"
+	"time"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	q := &queue.Queue{}
-	q.Enqueue("k")
+	webArchiveAccess := true
+	if godotenv.Load() != nil {
+		fmt.Println("Error loading .env file. No access to web archive.")
+		webArchiveAccess = false
+	}
+
+	db := DatabaseConnection{access: webArchiveAccess, uri: "", client: nil, collection: nil}
+	db.connect()
+
+	crawled := CrawledSet{data: make(map[uint64]bool)}
+	seed := "https://www.cc.gatetech.edu/"
+	queue := Queue{totalQueued: 0, number: 0, elements: make([]string, 0)}
+
+	ticker := time.NewTicker(1 * time.Minute)
+	done := make(chan bool)
+	crawlerStats := CrawledStats{pagesPerMinute: "0 0\n", crawledRatioPerMinute: "0 0\n", startTime: time.Now()}
+
+	// Tick every minute
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			case t := <-ticker.C:
+				crawlerStats.update(&crawled, &queue, t)
+			}
+		}
+	}()
+	queue.enqueue(seed)
+	url := queue.dequeue()
+	crawled.add(url)
+	c := make(chan []byte)
+
+	go fetchPage(url, c)
+
+	content := <-c
+	parseHTML(url, content, &queue, &crawled, &db)
+
+	for queue.size() > 0 && crawled.size() < 5000 {
+		url := queue.dequeue()
+		crawled.add(url)
+
+		go fetchPage(url, c)
+		content := <-c
+		if len(content) == 0 {
+			continue
+		}
+		go parseHTML(url, content, &queue, &crawled, &db)
+	}
+	ticker.Stop()
+	done <- true
+	db.disconnect()
+	fmt.Println("\n------------CRAWLER STATS------------")
+	fmt.Printf("Total queued: %d\n", queue.totalQueued)
+	fmt.Printf("To be crawled (Queue) size: %d\n", queue.size())
+	fmt.Printf("Crawled size: %d\n", crawled.size())
+	crawlerStats.print()
 }
