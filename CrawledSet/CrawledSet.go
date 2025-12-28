dc13839d90dbbdf81743a75104e336d5c0fcf8c2
Kanishk Jain <kanishkjain03082005@gmail.com>
2025-12-29 00:58:17 +0530

    implemented crawled set

---
+++ CrawledSet/CrawledSet.go
@@ -0,0 +1,37 @@
+package crawledset
+
+import (
+	"hash/fnv"
+	"sync"
+)
+
+type CrawledSet struct {
+	data   map[uint64]bool // map [hash -> true]
+	number int             // size of set
+	mu     sync.Mutex
+}
+
+func (c *CrawledSet) Add(url string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.data[hashUrl(url)] = true
+	c.number++
+}
+
+func (c *CrawledSet) Contains(url string) bool {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.data[hashUrl(url)]
+}
+
+func (c *CrawledSet) Size() int {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.number
+}
+
+func hashUrl(url string) uint64 { // non-cryptographic hash
+	h := fnv.New64a()
+	h.Write([]byte(url))
+	return h.Sum64()
+}
